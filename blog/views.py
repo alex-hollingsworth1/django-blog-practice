c8292de99b236c382d9eb3b19db0cd919fbece49
alex-hollingsworth1 <alex@hsoundworks.co.uk>
2025-10-25 17:38:17 +0100

    Initial commit. Setting up practice Django Blog Project.

---
+++ blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def starting_page(request):
+    pass
+
+
+def posts(request):
+    pass
+
+
+def post_detail(request):
+    pass
