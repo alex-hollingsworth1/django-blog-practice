30dd9a2ecba008e9cf40c9259509b827c68e0bc8
alex-hollingsworth1 <alex@hsoundworks.co.uk>
2025-11-06 19:16:41 +0000

    Load SECRET_KEY from environment variables

--- my_site/settings.py
+++ my_site/settings.py
@@ -10,7 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+# Load environment variables from .env
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = (
-    "django-insecure-rizr@)g%rbn-k@#*%#0-9m-)*hxw1cj!0z0#-a&vm$#8&hy4(!"
+    os.environ.get('DJANGO_SECRET_KEY', 'unsafe-default')
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
